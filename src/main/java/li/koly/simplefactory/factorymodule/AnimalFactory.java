85368fccf345d5cd5b5c96b9a2f3657b1fd04a3a
kolyjjj <me.koly.li@gmail.com>
2015-11-24 13:31:39 +0800

    use reflection to build a relation from input name to class name

--- src/main/java/li/koly/simplefactory/factorymodule/AnimalFactory.java
+++ src/main/java/li/koly/simplefactory/factorymodule/AnimalFactory.java
@@ -2,11 +2,17 @@ package li.koly.simplefactory.factorymodule;
 
 public class AnimalFactory {
     public static Animal buildAnimal(String animalName) {
-        if ("dog".equals(animalName.toLowerCase())) {
-            return new Dog();
-        }
-        if ("cat".equals(animalName.toLowerCase())) {
-            return new Cat();
+        String temp = animalName.toLowerCase();
+        String result = temp.substring(0, 1).toUpperCase() + temp.substring(1);
+        try {
+            Class<?> animalClass = AnimalFactory.class.forName("li.koly.simplefactory.factorymodule." + result);
+            return (Animal) animalClass.newInstance();
+        } catch (ClassNotFoundException e) {
+            throw new AnimalNotFoundException("cannot find animal class with name: " + animalName);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
 
         throw new AnimalNotFoundException("the expected animal is " + animalName);
